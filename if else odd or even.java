403e12a09c923416445ddeadd9caa6be02debc7c
RajeshwariSelvaraj <rajeshwariselvaraj929@gmail.com>
2026-01-12 10:09:49 +0530

    Add files via upload

---
+++ if else odd or even.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	  int number = 25;   
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
